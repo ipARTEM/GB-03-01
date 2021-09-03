af7d15ca6b0486d4110dc83007afd2cf70f047e5
Artem <a.khimin@yandex.ru>
2021-09-03 21:42:29 +0300

    Microservice

---
+++ FirstMicroservice/Controllers/TemperatureController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstMicroservice.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TemperatureController : ControllerBase
+    {
+        private readonly List<Temperature> _temperatures;
+
+        public TemperatureController(List<Temperature> temperature)
+        {
+            _temperatures = temperature;
+
+        }
+
+
+        [HttpGet]
+        public IActionResult Read() =>
+            Ok(_temperatures.OrderBy(item => item.Date));
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Temperature temperature)
+        {
+            if (_temperatures.TrueForAll(item=>item.Date!=temperature.Date))
+            _temperatures.Add(temperature);
+
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult Update([FromBody] Temperature temperature)
+        {
+            _temperatures.FirstOrDefault(item => item.Date == temperature.Date)?.CopyData(temperature);
+
+            return Ok();
+        }
+
+
+        [HttpDelete]
+        public IActionResult Delete([FromBody] Temperature temperature)
+        {
+            if(temperature!=null)
+           _temperatures.Remove( temperature);
+            return Ok();
+        }
+
+
+    }
+}
---
+++ FirstMicroservice/Temperature.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstMicroservice
+{
+    public class Temperature
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+
+        public void CopyData(Temperature temperature)
+        {
+            Date = temperature.Date;
+            TemperatureC = temperature.TemperatureC;
+            Summary = temperature.Summary;
+        }
+
+        public override bool Equals(object obj) =>
+            obj is Temperature temperature ? this == temperature : false;
+
+        public override int GetHashCode() =>
+            ~Date.GetHashCode() * TemperatureC.GetHashCode();
+
+        public static bool operator ==(Temperature t1, Temperature t2)
+        {
+            if (ReferenceEquals(t1, null) || ReferenceEquals(t2, null))
+                return false;
+            return t1.Date == t2.Date &&
+                t1.TemperatureC == t2.TemperatureC &&
+                t1.Summary == t2.Summary;
+
+        }
+
+        public static bool operator !=(Temperature t1, Temperature t2) =>
+            !(t1 == t2);
+
+
+    }
+}
